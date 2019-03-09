5fdc89227578a32cefac88f3f76ccfcf8bd1e635
nawendusingh <nawendu6.72f@gmail.com>
2019-03-09 11:46:58 +0530

    Add files via upload
    14. Longest Common Prefix

---
+++ 14_lcp.py
@@ -0,0 +1,36 @@
+def lcp(arr):
+    n = len(arr)
+    result = ''
+    if n == 0:
+        return result
+    minlen = len(min(arr, key=len))
+
+
+# minlen will give error if there is an empty string in list
+# so added condition for empty string
+
+    for i in range(minlen):
+        current = arr[0][i]
+        for j in range(1, n):
+            if(arr[j][i] != current):
+                return result
+# result moved to outer for loop because current  was
+# getting added twice to resultinside inner for loop
+        result += current
+    return result
+
+
+print(lcp(['flight', 'flow', 'flower']))
+
+# algorithm:
+# find the len of minimum string in the list
+# for this min length
+# compare character by character the first string to all
+# first loop is for 1st word
+# 2nd loop is for strings from index 2 to last
+# if they all match add to result variable
+# if they dont return result
+
+
+# Runtime: 44 ms, faster than 47.64% of Python3
+# Memory Usage: 13.1 MB, less than 5.10% of Python3
